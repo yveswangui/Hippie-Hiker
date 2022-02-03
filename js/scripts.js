6d7cbe52c653fa985bad5519fa5910f363657719
yveswangui <yveswangui@gmail.com>
2022-02-03 16:39:43 +0300

    cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
     $(".card").hover(function(){
         if($(this).hasClass("active")){
-            $(".card .bottom").slideUp(function(){
+            ($(".card .bottom").slideUp(function(){
                 $(".card").removeClass("active");
-            });
+            }));
         }else{
             $(".card").addClass("active");
             $(".card .bottom").stop().slideDown();
         }
+    
     });
 });
