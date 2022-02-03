178e91461eda3a6b6d284277611e3602a3eaf766
yveswangui <yveswangui@gmail.com>
2022-02-03 13:23:55 +0300

    slideUp and Down functions on hover

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".card").hover(function(){
+        if($(this).hasClass("active")){
+            $(".card .bottom").slideUp(function(){
+                $(".card").removeClass("active");
+            });
+        }else{
+            $(".card").addClass("active");
+            $(".card .bottom").stop().slideDown();
+        }
+    });
+});
