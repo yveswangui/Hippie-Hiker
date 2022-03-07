62c7246cec9e405ac634aca20a81652931b67da2
yveswangui <yveswangui@gmail.com>
2022-03-07 16:21:14 +0300

    prevent page from redirecting

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+    $("form#my-button").submit(function(e){
+        e.preventDefault();
+    });
+});
+$(document).ready(function(){
     $(".card").hover(function(){
         if($(this).hasClass("active")){
             ($(".card .bottom").slideUp(function(){
